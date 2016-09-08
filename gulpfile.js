ce536d057125440791cf76133b9c93ff063de846
cmpereira <cmpereira50@gmail.com>
2016-09-08 12:00:17 -0300

    modificando arquivo gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,27 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-//var uglify = require('gulp-uglify');
+var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-
-gulp.task('default',['sass','watch','serve']);
-
-//my task sass
-gulp.task('sass', function () {
-  return gulp.src('*.scss')
-    //.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
+var sass        = require('gulp-sass');
+//var uglify = require('gulp-uglify'); minificador js
 
 
-gulp.task('watch', function() {
-  gulp.watch('/*.scss', ['sass']);
-  gulp.watch('js/*.js', ['js']);
-  gulp.watch('*.html').on('change', browserSync.reload);
-});
-
+gulp.task('default', ['serve', 'sass']);
+// Static Server + watching scss/html files
 gulp.task('serve', function() {
+
     browserSync.init({
-        server: {
-            baseDir: "*./"
-        }
+        server: "./"
     });
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+   	gulp.watch("js/*.js");
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+
 });
 
--- js/jquery.js
+++ js/jquery.js
@@ -1,1 +1,16 @@
-$(document).ready(function(){$("h1").click(function(c){$("h1").html("<h2>Helllo</h2>")}),$("button").click(function(c){$(this).css("color","yellow")});var c=$("#bird").html();$("button").click(function(n){$("#bird").html(c+" This is me")}),$("button").click(function(c){$(".container").css({transition:"2s",background:"purple"})})});
+$(document).ready(function(){
+	$("h1").click(function(c){
+		$("h1").html("<h2>Hello</h2>")
+	}),
+	$("button").click(function(c){
+		$(this).css("color","yellow")
+	});
+
+	var c=$("#bird").html();
+	$("button").click(function(n){
+		$("#bird").html(" h is me")
+	}),
+	$("button").click(function(c){
+		$(".container").css({transition:"2s",background:"purple"})
+	})
+});
